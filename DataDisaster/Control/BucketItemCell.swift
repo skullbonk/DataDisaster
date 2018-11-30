606406cbced39cffaebc3b38c011860507cccbfc
Skullbonk <reagan.fairbanks@gmail.com>
2018-11-30 11:57:27 -0700

    Initial commit

---
+++ DataDisaster/Control/BucketItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  BucketItemCell.swift
+//  DataDisaster
+//
+//  Created by Fairbanks, Reagan on 11/30/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DataDisaster/Model/BucketItem.swift
@@ -0,0 +1,11 @@
+//
+//  BucketItem.swift
+//  DataDisaster
+//
+//  Created by Fairbanks, Reagan on 11/30/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+
+
